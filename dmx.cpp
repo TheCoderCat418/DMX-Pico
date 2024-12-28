2c649d19a732dd0eb67868866897be90329cee1d
Elliott Herman <89421400+TheCoderCat418@users.noreply.github.com>
2024-12-27 19:00:35 -0500

    Fixup

--- dmx.cpp
+++ dmx.cpp
@@ -15,14 +15,12 @@ int main()
     stdio_init_all();
     sleep_ms(4000);
 
-    // PIO Blinking example
     PIO pio = pio0;
     uint offset = pio_add_program(pio, &dmx_program);
     printf("VER:0.0 Loaded program at %d\n", offset);
     
     blink_pin_forever(pio, 0, offset, 14, 3);
     pio_sm_put(pio, 0, 0b10000001010000100010010000011000);
-    // For more pio examples see https://github.com/raspberrypi/pico-examples/tree/master/pio
 
     while (true) {
         printf("%d", pio_sm_get_tx_fifo_level(pio, 0));
