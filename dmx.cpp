052ab9780f8d5d53ceb1966cefb661a10bbb0ecd
Elliott Herman <89421400+TheCoderCat418@users.noreply.github.com>
2025-01-12 22:28:28 -0600

    idk

--- dmx.cpp
+++ dmx.cpp
@@ -1,9 +1,44 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
-#include "hardware/dma.h"
+
+// USB register definitions from pico-sdk
+#include "hardware/regs/usb.h"
+// USB hardware struct definitions from pico-sdk
+#include "hardware/structs/usb.h"
+// For interrupt enable and numbers
+#include "hardware/irq.h"
+// For resetting the USB controller
+#include "hardware/resets.h"
 
 #include "dmx.pio.h"
+
+// START USB TESTING
+void usb_main(){
+    // Reset the USB Controller
+    reset_unreset_block_num_wait_blocking(RESET_USBCTRL);
+
+
+
+
+}
+
+// END USB TESTING
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
     dmx_program_init(pio, sm, offset, pin);
@@ -22,8 +57,7 @@ int main()
     blink_pin_forever(pio, 0, offset, 14, 3);
     pio_sm_put(pio, 0, 0b10000001010000100010010000011000);
 
-    while (true) {
-        printf("%d", pio_sm_get_tx_fifo_level(pio, 0));
-        sleep_ms(1000);
+    for(int i = 0; i< 128; i++) {
+        pio_sm_put_blocking(pio, 0, 0b10000001010000100010010000011000);
     }
 }
